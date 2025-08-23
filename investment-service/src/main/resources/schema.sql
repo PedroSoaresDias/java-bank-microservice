621c4a7eb69e51249f534f48a6ff42f33a1a9326
Pedro Soares Dias <pedroferraridias@gmail.com>
2025-08-23 09:51:13 -0300

    Criando o container Docker para o módulo de investimentos e implementando no Docker Compose

--- investment-service/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS investment_wallet (
-    id SERIAL PRIMARY KEY NOT NULL,
-    pix VARCHAR(255) UNIQUE NOT NULL,
-    balance DECIMAL(19, 2) NOT NULL DEFAULT 0.00,
-    tax DECIMAL(5, 2) NOT NULL,
-    initial_deposit DECIMAL(19, 2) NOT NULL,
-    user_id BIGINT NOT NULL
-);
